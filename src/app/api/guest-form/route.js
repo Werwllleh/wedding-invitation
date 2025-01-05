9675d75c70e8234b2111e6727ce46b7e5a8d71f6
Alexey A <allvxm4@gmail.com>
2025-01-05 17:10:10 +0300

    mail send st2

--- src/app/api/guest-form/route.js
+++ src/app/api/guest-form/route.js
@@ -1,34 +1,37 @@
 import {NextResponse} from 'next/server';
 import nodemailer from "nodemailer";
 
-/*const transporter = nodemailer.createTransport({
-  host: 'smtp.ethereal.email',
-  port: 587,
-  auth: {
-    user: 'itzel.conroy@ethereal.email',
-    pass: 'W58sJEJ4uaeuJ5Nk74'
-  }
-});*/
 
-const transporter = nodemailer.createTransport({
+
+
+/*const transporter = nodemailer.createTransport({
   service: "yandex",
   host: "smtp.yandex.ru",
   port: 465,
   secure: true,
   auth: {
-    user: process.env.NEXT_PUBLIC_YANDEX_LOGIN,
+    user: 'cheshire778',
     pass: process.env.NEXT_PUBLIC_YANDEX_PASS
   }
-});
+});*/
 
 
 export async function POST(req) {
   try {
 
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: 'erwin.vandervort96@ethereal.email',
+        pass: '85ZyP7U6XtAWYA84Uu'
+      }
+    });
+
     const data = await req.json();
 
     await transporter.sendMail({
-      from: process.env.NEXT_PUBLIC_YANDEX_LOGIN, // sender address
+      from: 'aa-invitation.vercel.app', // sender address
       to: "crj-100@yandex.ru", // list of receivers
       subject: "Ответ на электронное пригласительное", // Subject line
       // text: "Hello world?", // plain text body
